63faab0a34d36d53df8d4c46daf634ef3a3403cf
aquinney <aquinney@gmail.com>
2020-02-08 13:09:51 -0700

    Lesson 5 no errors - JS left todo

--- Lesson-5/js/scripts.js
+++ Lesson-5/js/scripts.js
@@ -15,10 +15,12 @@ function toggleMenu() {
 }
 
 let options = {
-    day:"numeric",
-    month:"numeric",
-    year:"numeric"
+    day: "numeric",
+    month: "numeric",
+    year: "numeric"
 };
 let lastDate = new Date();
 document.getElementById('lastDate').textContent = lastDate.toLocaleDateString("en-US", options);
-document.getElementById('lastTime').textContent = lastDate.toLocaleTimeString("en-US", {hour12:true});
+document.getElementById('lastTime').textContent = lastDate.toLocaleTimeString("en-US", {
+    hour12: true
+});
