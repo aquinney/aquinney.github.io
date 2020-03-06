b3d69edd55b795dd1effd56eed5bf5cb934e7e29
aquinney <aquinney@gmail.com>
2020-03-05 19:46:00 -0700

    stormcenter submit button update

--- Lesson-8/js/stormcenter.js
+++ Lesson-8/js/stormcenter.js
@@ -1,3 +1,13 @@
 function adjustSeverity(severity) {
     document.getElementById("stormseverity").innerHTML = severity;
 }
+function InvalidMsg(textbox) {
+
+    if(textbox.validity.patternMismatch){
+       textbox.setCustomValidity('Please enter your full name');
+   }    
+   else {
+       textbox.setCustomValidity('');
+   }
+   return true;
+}
